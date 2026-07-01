6d2f9af77b332704876cd4247542892fc6af14c9
Jenish <jpat534@gmail.com>
2026-07-01 18:10:38 +1000

    Added Podium Graph as a component

---
+++ app/components/PodiumGraph.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+import React from 'react';
+
+
+const PodiumGraph = () => {
+    return (
+        <h1 className="text-2xl text-center text-white p-2 m-2">Prodium Graph</h1>
+    )
+}
+
+export default PodiumGraph
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
-import Image from "next/image";
+'use client';
+import React from 'react';
+import PodiumGraph from './components/PodiumGraph';
 
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center py-32 px-16 bg-white dark:bg-black sm:items-start">
         <h1 className="text-5xl font-bold text-center p-2 m-2">Bet Leaderboard</h1>
+          <PodiumGraph />
         <h2 className="text-2xl font-semibold text-center p-2 m-2">Leaderboard</h2>
         <h2 className="text-2xl font-semibold text-center p-2 m-2">Graphs</h2>
         <h2 className="text-2xl font-semibold text-center p-2 m-2">Quarterly Report</h2>
