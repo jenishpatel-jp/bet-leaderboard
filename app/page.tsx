276e88f2c2b6645619fe6b98b48404f7e0a14dfc
Jenish <jpat534@gmail.com>
2026-07-02 20:38:49 +1000

    Centered the graph

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,8 @@ import PodiumGraph from './components/charts/PodiumGraph';
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black border-2">
-      <main className="flex flex-1 w-full max-w-3xl flex-col justify-center bg-white dark:bg-black border-2">
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <main className="flex flex-1 w-full max-w-3xl flex-col justify-center items-center bg-white dark:bg-black border-2">
         <h1 className="text-5xl font-bold text-center p-2 m-2">Bet Leaderboard</h1>
         <PodiumGraph />
         <h2 className="text-2xl font-semibold text-center p-2 m-2">Leaderboard</h2>
