20600f735eaffe6382c3ae29a879e826649c7aa8
Jenish <jpat534@gmail.com>
2026-07-06 18:05:19 +1000

    Updated the prisma ts file

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,12 +1,11 @@
 import { PrismaClient } from "@/lib/generated/prisma/client";
 import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
-import Database from "better-sqlite3";
 
-const db = new Database("prisma/dev.db");
+const adapter = new PrismaBetterSqlite3({
+  url: "file:./prisma/dev.db",
+});
 
-const adapter = new PrismaBetterSqlite3(db);
-
-const globalForPrisma = globalThis as {
+const globalForPrisma = globalThis as unknown as {
   prisma?: PrismaClient;
 };
 
