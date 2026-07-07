0478fa2c3f6e79132b3657322da990439f2e028d
Jenish <jpat534@gmail.com>
2026-07-07 17:32:30 +1000

    Updated the prisma.ts file to use the .env file

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,8 +1,14 @@
 import { PrismaClient } from "@/lib/generated/prisma/client";
 import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
 
+const databaseUrl = process.env.DATABASE_URL;
+
+if (!databaseUrl) {
+  throw new Error("DATABASE_URL is not defined.")
+}
+
 const adapter = new PrismaBetterSqlite3({
-  url: "file:./prisma/dev.db",
+  url: databaseUrl,
 });
 
 const globalForPrisma = globalThis as unknown as {
