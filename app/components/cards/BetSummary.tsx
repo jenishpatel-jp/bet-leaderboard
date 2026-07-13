420482a6e2fc8ec973fc5662b6e9e5858541a266
Jenish <jpat534@gmail.com>
2026-07-13 21:23:34 +1000

    Added a table component

---
+++ app/components/cards/BetSummary.tsx
@@ -0,0 +1,35 @@
+"use client"
+
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+
+const BetSummary = () => {
+    return (
+        <Table className="border-2 w-1/2 mx-auto">
+            <TableHeader>
+                <TableRow>
+                <TableHead className="w-25">Invoice</TableHead>
+                <TableHead>Status</TableHead>
+                <TableHead>Method</TableHead>
+                <TableHead className="text-right">Amount</TableHead>
+                </TableRow>
+            </TableHeader>
+            <TableBody>
+                <TableRow>
+                <TableCell className="font-medium">INV001</TableCell>
+                <TableCell>Paid</TableCell>
+                <TableCell>Credit Card</TableCell>
+                <TableCell className="text-right">$250.00</TableCell>
+                </TableRow>
+            </TableBody>
+        </Table>
+    )
+}
+
+export default BetSummary
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import BetSummary from './components/cards/BetSummary';
 import LineGraph from './components/charts/LineGraph';
 import PodiumGraph from './components/charts/PodiumGraph';
 import { getPlayerProfit } from '@/lib/stats/profit';
@@ -20,13 +21,21 @@ export default async function Home(  ) {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-white font-sans dark:bg-black">
       <main className="flex flex-1 w-full flex-col justify-center items-center bg-background dark:bg-black border-2">
+        
         <h1 className="text-5xl font-bold text-center p-2 m-2 text-foreground">Bet Leaderboard</h1>
+        
         <div className="flex w-full h-screen bg-background items-center justify-center ">
           <LineGraph chartData={roundProfitData} />
         </div>
+        
+        <div className="flex w-full h-screen bg-background border-2 items-center justify-center">
+          <BetSummary />
+        </div>
+
         <div className="flex w-full h-screen bg-background items-center justify-center">
           <PodiumGraph chartData={podiumChartData} />
         </div>
+
 
       </main>
     </div>
