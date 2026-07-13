e2056091fd6cf7bcc398852cc06670223b5310b7
Jenish <jpat534@gmail.com>
2026-07-13 21:34:37 +1000

    Added additional tablehead and table row elements

--- app/components/cards/BetSummary.tsx
+++ app/components/cards/BetSummary.tsx
@@ -11,21 +11,61 @@ import {
 
 const BetSummary = () => {
     return (
-        <Table className="border-2 w-1/2 mx-auto">
+        <Table className="border-2 w-1/2 h-110 mx-auto">
             <TableHeader>
                 <TableRow>
-                <TableHead className="w-25">Invoice</TableHead>
-                <TableHead>Status</TableHead>
-                <TableHead>Method</TableHead>
-                <TableHead className="text-right">Amount</TableHead>
+                    <TableHead>Player</TableHead>
+                    <TableHead>Total Bets Placed</TableHead>
+                    <TableHead>Total Valid Bets</TableHead>
+                    <TableHead>Wins</TableHead>
+                    <TableHead>Losses</TableHead>
+                    <TableHead>Voids</TableHead>
+                    <TableHead>Cashed out</TableHead>
+                    <TableHead>Total Win %</TableHead>
+                    <TableHead>Total Win $</TableHead>
+                    <TableHead>Total Losses $</TableHead>
+                    <TableHead>Profit</TableHead>
                 </TableRow>
             </TableHeader>
             <TableBody>
                 <TableRow>
-                <TableCell className="font-medium">INV001</TableCell>
-                <TableCell>Paid</TableCell>
-                <TableCell>Credit Card</TableCell>
-                <TableCell className="text-right">$250.00</TableCell>
+                    <TableCell>Shawry</TableCell>
+                    <TableCell>10</TableCell>
+                    <TableCell>11</TableCell>
+                    <TableCell>$250.00</TableCell>
+                    <TableCell>$250.00</TableCell>
+                    <TableCell>$250.00</TableCell>
+                    <TableCell>$250.00</TableCell>
+                    <TableCell>$250.00</TableCell>
+                    <TableCell>$250.00</TableCell>
+                    <TableCell>$250.00</TableCell>
+                    <TableCell>$250.00</TableCell>
+                </TableRow>
+                <TableRow>
+                    <TableCell>Shaz</TableCell>
+                    <TableCell>10</TableCell>
+                    <TableCell>11</TableCell>
+                    <TableCell>$250.00</TableCell>
+                    <TableCell>$250.00</TableCell>
+                    <TableCell>$250.00</TableCell>
+                    <TableCell>$250.00</TableCell>
+                    <TableCell>$250.00</TableCell>
+                    <TableCell>$250.00</TableCell>
+                    <TableCell>$250.00</TableCell>
+                    <TableCell>$250.00</TableCell>
+                </TableRow>
+                <TableRow>
+                    <TableCell>JP</TableCell>
+                    <TableCell>10</TableCell>
+                    <TableCell>11</TableCell>
+                    <TableCell>$250.00</TableCell>
+                    <TableCell>$250.00</TableCell>
+                    <TableCell>$250.00</TableCell>
+                    <TableCell>$250.00</TableCell>
+                    <TableCell>$250.00</TableCell>
+                    <TableCell>$250.00</TableCell>
+                    <TableCell>$250.00</TableCell>
+                    <TableCell>$250.00</TableCell>
                 </TableRow>
             </TableBody>
         </Table>
