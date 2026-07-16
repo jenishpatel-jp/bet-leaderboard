07819f71fbe0b19319f00718e59436dbff97ed7e
Jenish <jpat534@gmail.com>
2026-07-16 18:07:24 +1000

    Completed the Betting Card component

--- app/components/cards/BettingCard.tsx
+++ app/components/cards/BettingCard.tsx
@@ -1,9 +1,7 @@
 import {
   Card,
-  CardAction,
   CardContent,
   CardDescription,
-  CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
@@ -14,19 +12,66 @@ import {
 const BettingCard = () => {
     return (
         <Card className="bg-background text-white w-1/6 h-1/2 rounded-3xl flex border-2">
+
             <CardHeader>
                 <CardTitle className="text-center text-3xl">1st Quarter</CardTitle>
                 <CardDescription className="text-center text-white">Opening Round - Round 6</CardDescription>
             </CardHeader>
-            <CardContent >
-                <div className="grid grid-cols-[1.4fr_repeat(4,1fr)] border-b px-4 py-3 text-sm font-medium *:text-white">
-                    <span>Player</span>
-                    <span className="text-center">Wins</span>
-                    <span className="text-center">Losses</span>
-                    <span className="text-center">Win %</span>
-                    <span className="text-right">Win $</span>
+
+            
+            <CardContent className="p-0">
+                {/* Header */}
+                <div className="grid grid-cols-[2fr_repeat(3,1fr)] border-b px-4 py-3 font-bold *:text-white text-xl">
+                    <div></div>
+                    <div className="text-center">Shawry</div>
+                    <div className="text-center">JP</div>
+                    <div className="text-center">Shaz</div>
                 </div>
+
+                {/* Wins */}
+                <div className="grid grid-cols-[2fr_repeat(3,1fr)] px-4 py-3 font-bold *:text-white text-lg">
+                    <div>Wins</div>
+                    <div className="text-center">12</div>
+                    <div className="text-center">10</div>
+                    <div className="text-center">8</div>
+                </div>
+
+                {/* Losses */}
+                <div className="grid grid-cols-[2fr_repeat(3,1fr)] px-4 py-3 font-bold *:text-white text-lg">
+                    <div>Losses</div>
+                    <div className="text-center">5</div>
+                    <div className="text-center">13</div>
+                    <div className="text-center">3</div>
+                </div>
+
+                {/* Cashed out */}
+                <div className="grid grid-cols-[2fr_repeat(3,1fr)] px-4 py-3 font-bold *:text-white text-lg">
+                    <div>Cash Out</div>
+                    <div className="text-center">1</div>
+                    <div className="text-center">2</div>
+                    <div className="text-center">0</div>
+                </div>
+
+                {/* Win % */}
+                <div className="grid grid-cols-[2fr_repeat(3,1fr)] px-4 py-3 font-bold *:text-white text-lg">
+                    <div>Win %</div>
+                    <div className="text-center">50%</div>
+                    <div className="text-center">13%</div>
+                    <div className="text-center">3%</div>
+                </div>
+
+                {/* Profit*/}
+                <div className="grid grid-cols-[2fr_repeat(3,1fr)] px-4 py-3 font-bold *:text-white text-lg">
+                    <div>Profit</div>
+                    <div className="text-center">$120</div>
+                    <div className="text-center">$40</div>
+                    <div className="text-center">$80</div>
+                </div>
+
             </CardContent>
+
+            
+
         </Card>
     )
 }
